60049b971a193a743919b9444bdd3cf5305be331
n9524726-star <n9524726@gmail.com>
2026-05-22 19:55:04 +0530

    config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 from os import getenv
 
-API_ID = int(os.environ.get("API_ID", "22215080"))  # Replace "123456" with your actual api_id or use .env
-API_HASH = os.environ.get("API_HASH", "6ab80ad5d78fee18fdd9b909edfbafd5")
+API_ID = int(os.environ.get("API_ID", "35524250"))  # Replace "123456" with your actual api_id or use .env
+API_HASH = os.environ.get("API_HASH", "98de9f02ef055387a1cf3eda6220a18e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "7841292070"))  # Your Telegram user ID
-SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "7841292070").split()))  # Space-separated user IDs
+OWNER_ID = int(os.environ.get("OWNER_ID", "5200919974"))  # Your Telegram user ID
+SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "5200919974").split()))  # Space-separated user IDs
 
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://sonickuwal_db_user:alfsMN6kIAt9qNQ1@cluster0.lqwg2ve.mongodb.net/?appName=Cluster0")##your mongo url eg: withmongodb+srv://xxxxxxx:xxxxxxx@clusterX.xxxx.mongodb.net/?retryWrites=true&w=majority
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002355909549"))  # Telegram channel ID (with -100 prefix)
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "--1003086072844"))  # Telegram channel ID (with -100 prefix)
 
-PREMIUM_LOGS = os.environ.get("PREMIUM_LOGS", "-1002355909549")  # Optional here you'll get all logs
+PREMIUM_LOGS = os.environ.get("PREMIUM_LOGS", "--1003086072844")  # Optional here you'll get all logs
